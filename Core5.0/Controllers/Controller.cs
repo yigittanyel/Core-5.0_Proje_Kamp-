5d371b53b9d492102ff4e34250b6572b069298c2
Yigit <tanyelyigit@gmail.com>
2022-02-24 14:28:31 +0300

    first job completed

---
+++ Core5.0/Controllers/Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core5._0.Controllers
+{
+    public class Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
